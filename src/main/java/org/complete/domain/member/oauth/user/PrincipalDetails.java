b3203b5b7a315da2c07c84c2a39718ac00fe39d5
minoolian <mw.lee@kakao.com>
2024-01-22 08:55:04 +0900

    feat: Authentication 인증 객체 초기 구성
    - OAuth2 로그인 시 Security Context에 저장할 Authentication 객체 추가

---
+++ src/main/java/org/complete/domain/member/oauth/user/PrincipalDetails.java
@@ -0,0 +1,23 @@
+package org.complete.domain.member.oauth.user;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+public class PrincipalDetails implements OAuth2User {
+
+    private String socialId;
+    private ProviderType providerType;
+    private RoleType roleType;
+    private Collection<GrantedAuthority> authorities;
+    private Map<String, Object> attributes;
+
+    @Override
+    public String getName() {
+        return socialId;
+    }
+}
