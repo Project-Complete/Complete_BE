a350e2fc5626e781ec131ea3effd746b11727278
minoolian <mw.lee@kakao.com>
2024-01-22 08:33:09 +0900

    feat: OAuth 로그인 Provider Enum Type 선언
    - 각 사용자의 로그인 Provider를 담는 Enum 추가

---
+++ src/main/java/org/complete/domain/member/oauth/user/ProviderType.java
@@ -0,0 +1,16 @@
+package org.complete.domain.member.oauth.user;
+
+public enum ProviderType {
+
+    GOOGLE("google", "구글 로그인"),
+    KAKAO("kakao", "카카오 로그인"),
+    NAVER("naver", "네이버 로그인");
+
+    final String providerId;
+    final String description;
+
+    ProviderType(String providerId, String description) {
+        this.providerId = providerId;
+        this.description = description;
+    }
+}
