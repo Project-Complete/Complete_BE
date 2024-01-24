03394c3aac514af0c4ec48f1029d1e396a5dc02c
minoolian <mw.lee@kakao.com>
2024-01-24 19:48:52 +0900

    feat: JWT Filter 추가
    - Security Filter단에 추가될 JWT 검증 필터

---
+++ src/main/java/org/complete/domain/member/filter/TokenAuthenticationFilter.java
@@ -0,0 +1,38 @@
+package org.complete.domain.member.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.complete.domain.member.oauth.token.AuthToken;
+import org.complete.domain.member.oauth.token.AuthTokenProvider;
+import org.complete.domain.member.util.HeaderUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+    private final AuthTokenProvider tokenProvider;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)  throws ServletException, IOException {
+
+        String tokenStr = HeaderUtils.getAccessToken(request);
+        AuthToken token = tokenProvider.convertAuthToken(tokenStr);
+
+        if (token.validate()) {
+            Authentication authentication = tokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
