b10ffbf2269f5277e5e0363167cf60399b04db71
minoolian <mw.lee@kakao.com>
2024-01-24 20:00:05 +0900

    feat: Header 조작 Utils 추가

---
+++ src/main/java/org/complete/domain/member/util/HeaderUtils.java
@@ -0,0 +1,23 @@
+package org.complete.domain.member.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class HeaderUtils {
+
+    public final static String HEADER_AUTHORIZATION = "Authorization";
+    public final static String TOKEN_PREFIX = "Bearer ";
+
+    public static String getAccessToken(HttpServletRequest request) {
+        String headerValue = request.getHeader(HEADER_AUTHORIZATION);
+
+        if (headerValue == null) {
+            return null;
+        }
+
+        if (headerValue.startsWith(TOKEN_PREFIX)) {
+            return headerValue.substring(TOKEN_PREFIX.length());
+        }
+
+        return null;
+    }
+}
