e717365a33f4ad86568541614bfdc717e93d8129
swoody1101@gmail.com <swoody1101@gmail.com>
2024-01-28 19:16:37 +0900

    feat: ReviewFood class 생성 및 필드 추가
    - 사용자가 주류와 어울리는 음식을 매칭한 정보를 담은 Review entity와 Food entity를 N:N 매핑하기 위한 ReviewFood class 생성
    - BaseEntity class 상속

---
+++ src/main/java/org/complete/challang/review/domain/entity/ReviewFood.java
@@ -0,0 +1,20 @@
+package org.complete.challang.review.domain.entity;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.complete.challang.common.domain.entity.BaseEntity;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@AttributeOverride(name = "id", column = @Column(name = "review_food_id"))
+@Entity
+public class ReviewFood extends BaseEntity {
+}
