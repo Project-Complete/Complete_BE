774edf0525ff22bf3a48c219533a208f05253449
swoody1101@gmail.com <swoody1101@gmail.com>
2024-01-28 19:22:04 +0900

    feat: DrinkManufacturer class 생성 및 필드 추가
    - 주류 제조사 정보를 담을 DrinkManufacturer class 생성
    - BaseEntity class 상속

---
+++ src/main/java/org/complete/challang/drink/domain/entity/DrinkManufacturer.java
@@ -0,0 +1,22 @@
+package org.complete.challang.drink.domain.entity;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.complete.challang.common.domain.entity.BaseEntity;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@AttributeOverride(name = "id", column = @Column(name = "drink_manufacturer_id"))
+@Entity
+public class DrinkManufacturer extends BaseEntity {
+
+    private String manufacturerName;
+}
