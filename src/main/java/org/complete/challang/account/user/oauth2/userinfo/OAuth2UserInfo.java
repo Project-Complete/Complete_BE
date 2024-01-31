4e7cf087320b2724aaaa12eea79d4092d645cf09
minoolian <mw.lee@kakao.com>
2024-01-31 12:45:02 +0900

    feat: OAuth2UserInfo -> User 변환 메서드

--- src/main/java/org/complete/challang/account/user/oauth2/userinfo/OAuth2UserInfo.java
+++ src/main/java/org/complete/challang/account/user/oauth2/userinfo/OAuth2UserInfo.java
@@ -1,5 +1,8 @@
 package org.complete.challang.account.user.oauth2.userinfo;
 
+import org.complete.challang.account.user.domain.entity.SocialType;
+import org.complete.challang.account.user.domain.entity.User;
+
 import java.util.Map;
 
 public abstract class OAuth2UserInfo {
@@ -21,4 +24,13 @@ public abstract class OAuth2UserInfo {
     public abstract String getEmail();
 
     public abstract String getImageUrl();
+
+    public User toEntity(SocialType socialType) {
+        return User.builder()
+                .socialId(getSocialId())
+                .email(getEmail())
+                .profileImageUrl(getImageUrl())
+                .socialType(socialType)
+                .build();
+    }
 }
