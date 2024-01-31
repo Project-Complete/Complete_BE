a7fe73a7207826271342b6bd9fee10d2890351f4
minoolian <mw.lee@kakao.com>
2024-01-31 12:44:28 +0900

    feat: User Entity -> CustomOAuth2User 변환 메서드

--- src/main/java/org/complete/challang/account/user/domain/entity/User.java
+++ src/main/java/org/complete/challang/account/user/domain/entity/User.java
@@ -3,11 +3,15 @@ package org.complete.challang.account.user.domain.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.complete.challang.account.user.oauth2.CustomOAuth2User;
 import org.complete.challang.common.domain.entity.BaseEntity;
 import org.complete.challang.review.domain.entity.Review;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @SuperBuilder
@@ -41,4 +45,14 @@ public class User extends BaseEntity {
     @Builder.Default
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Review> reviews = new ArrayList<>();
+
+    public CustomOAuth2User toOAuth2User(Map<String, Object> attributes, String nameAttributeKey){
+        return CustomOAuth2User.builder()
+                .email(this.email)
+                .roleType(this.roleType)
+                .authorities(Collections.singletonList(new SimpleGrantedAuthority(roleType.getRole())))
+                .attributes(attributes)
+                .nameAttributeKey(nameAttributeKey)
+                .build();
+    }
 }
