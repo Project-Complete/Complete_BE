c9a00302b7f25ff7c73b3f773d337097aafa8212
minoolian <mw.lee@kakao.com>
2024-01-31 12:46:17 +0900

    fix: import providerType -> socialType으로 수정

--- src/main/java/org/complete/challang/account/user/oauth2/userinfo/OAuth2UserInfoFactory.java
+++ src/main/java/org/complete/challang/account/user/oauth2/userinfo/OAuth2UserInfoFactory.java
@@ -1,16 +1,16 @@
 package org.complete.challang.account.user.oauth2.userinfo;
 
+import org.complete.challang.account.user.domain.entity.SocialType;
 import org.complete.challang.account.user.oauth2.userinfo.impl.GoogleOAuth2UserInfo;
 import org.complete.challang.account.user.oauth2.userinfo.impl.KakaoOAuth2UserInfo;
 import org.complete.challang.account.user.oauth2.userinfo.impl.NaverOAuth2UserInfo;
-import org.complete.domain.member.oauth.domain.ProviderType;
 
 import java.util.Map;
 
 public class OAuth2UserInfoFactory {
 
-    public static OAuth2UserInfo getOAuth2UserInfo(ProviderType providerType, Map<String, Object> attributes) {
-        return switch (providerType) {
+    public static OAuth2UserInfo getOAuth2UserInfo(SocialType socialType, Map<String, Object> attributes) {
+        return switch (socialType) {
             case KAKAO -> new KakaoOAuth2UserInfo(attributes);
             case GOOGLE -> new GoogleOAuth2UserInfo(attributes);
             case NAVER -> new NaverOAuth2UserInfo(attributes);
