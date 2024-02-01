73237f646521cfb029c8f11c23e5e3d1da8f3fd9
minoolian <mw.lee@kakao.com>
2024-02-01 13:10:04 +0900

    feat: OAuth2 인증 객체 추가
    - DefaultOAuth2User를 상속한 Custom 클래스
    
    Co-authored-by: swoody1101 <swoody1101@gmail.com>

---
+++ src/main/java/org/complete/challang/account/oauth2/CustomOAuth2User.java
@@ -0,0 +1,27 @@
+package org.complete.challang.account.oauth2;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.complete.challang.account.user.domain.entity.RoleType;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+public class CustomOAuth2User extends DefaultOAuth2User {
+
+    private String email;
+    private RoleType roleType;
+
+    @Builder
+    public CustomOAuth2User(Collection<? extends GrantedAuthority> authorities,
+                            Map<String, Object> attributes,
+                            String nameAttributeKey,
+                            String email, RoleType roleType) {
+        super(authorities, attributes, nameAttributeKey);
+        this.email = email;
+        this.roleType = roleType;
+    }
+}
