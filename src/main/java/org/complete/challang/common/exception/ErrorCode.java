6b5a6bfc21e22f7dc4bd15ed0c835b6e3aa89867
minoolian <mw.lee@kakao.com>
2024-02-05 10:59:36 +0900

    feat: 주류 상세조회 서비스 로직 추가
    - 주류 상세조회 서비스 로직
    - 존재하지 않는 주류 조회 ErrorCode

--- src/main/java/org/complete/challang/common/exception/ErrorCode.java
+++ src/main/java/org/complete/challang/common/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    USER_NOT_FOUND("유저 정보가 존재하지 않습니다", HttpStatus.NOT_FOUND);
+    USER_NOT_FOUND("유저 정보가 존재하지 않습니다", HttpStatus.NOT_FOUND),
+    DRINK_NOT_FOUND("주류 정보가 존재하지 않습니다", HttpStatus.NOT_FOUND);
 
     private final String message;
     private final HttpStatus httpStatus;
---
+++ src/main/java/org/complete/challang/drink/service/DrinkService.java
@@ -0,0 +1,20 @@
+package org.complete.challang.drink.service;
+
+import lombok.RequiredArgsConstructor;
+import org.complete.challang.common.exception.ApiException;
+import org.complete.challang.common.exception.ErrorCode;
+import org.complete.challang.drink.domain.entity.Drink;
+import org.complete.challang.drink.repository.DrinkRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class DrinkService {
+
+    private final DrinkRepository drinkRepository;
+
+    public Drink findDetailDrink(Long drinkId) {
+        return drinkRepository.findById(drinkId).orElseThrow(() -> new ApiException(ErrorCode.DRINK_NOT_FOUND));
+
+    }
+}
