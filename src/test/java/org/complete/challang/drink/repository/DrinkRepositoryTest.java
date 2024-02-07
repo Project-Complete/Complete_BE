884f5a70f8527d9c3fddc239ec4aba7fae924f57
minoolian <mw.lee@kakao.com>
2024-02-08 08:43:00 +0900

    test: 별도 Test 외부설정파일 추가
    - test application.yml 추가
    - DrinkRepositoryTest에 profile 지정 삭제

--- src/test/java/org/complete/challang/drink/repository/DrinkRepositoryTest.java
+++ src/test/java/org/complete/challang/drink/repository/DrinkRepositoryTest.java
@@ -5,11 +5,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ActiveProfiles("local")
 @DataJpaTest
 public class DrinkRepositoryTest {
 
