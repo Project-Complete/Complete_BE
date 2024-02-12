55dbc0dde47a0e444b84c1b5563e6b5773f4c1d7
minoolian <mw.lee@kakao.com>
2024-02-12 11:54:19 +0900

    feat: situation, taste 통계 builder 및 getter 추가

--- src/main/java/org/complete/challang/drink/domain/entity/TasteStatistic.java
+++ src/main/java/org/complete/challang/drink/domain/entity/TasteStatistic.java
@@ -1,7 +1,12 @@
 package org.complete.challang.drink.domain.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.*;
 
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Embeddable
 public class TasteStatistic {
 
