4311aaae0b6ed93a3135f58979a042c10498e224
minoolian <mw.lee@kakao.com>
2024-02-14 14:53:52 +0900

    fix: User 테이블명 수정
    - h2 test시 에러 미연에 방지

--- src/main/java/org/complete/challang/account/user/domain/entity/User.java
+++ src/main/java/org/complete/challang/account/user/domain/entity/User.java
@@ -19,6 +19,7 @@ import java.util.Map;
 @AllArgsConstructor
 @AttributeOverride(name = "id", column = @Column(name = "user_id"))
 @Entity
+@Table(name = "users")
 public class User extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
