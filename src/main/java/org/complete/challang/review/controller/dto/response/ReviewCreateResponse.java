010d5925658f731cc03049c78412718db4bca4fa
swoody1101@gmail.com <swoody1101@gmail.com>
2024-02-14 15:50:12 +0900

    feat: ReviewCreateResponse dto class 생성
    - ReviewCreateResponse dto class 생성
    - reviewCreatedDate 변수 String 형식으로 포멧
    - toDto 함수 생성

---
+++ src/main/java/org/complete/challang/review/controller/dto/response/ReviewCreateResponse.java
@@ -0,0 +1,34 @@
+package org.complete.challang.review.controller.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Getter;
+import org.complete.challang.review.domain.entity.Review;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ReviewCreateResponse {
+
+    private Long reviewId;
+    private String imageUrl;
+    private String content;
+    private float rating;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime reviewCreatedDate;
+
+    public static ReviewCreateResponse toDto(final Review review) {
+        return ReviewCreateResponse.builder()
+                .reviewId(review.getId())
+                .imageUrl(review.getImageUrl())
+                .content(review.getContent())
+                .rating(review.getRating())
+                .reviewCreatedDate(review.getCreatedDate())
+                .build();
+    }
+}
