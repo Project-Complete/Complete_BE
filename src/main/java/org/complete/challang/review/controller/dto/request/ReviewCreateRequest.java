dc7f5855ac1a880139b95c8c96333b53b9d81594
swoody1101@gmail.com <swoody1101@gmail.com>
2024-02-14 15:49:08 +0900

    feat: ReviewCreateRequest dto class 생성
    - ReviewCreateRequest dto class 생성
    - 각 변수마다 validation 설정
    - toEntity 함수 생성

---
+++ src/main/java/org/complete/challang/review/controller/dto/request/ReviewCreateRequest.java
@@ -0,0 +1,59 @@
+package org.complete.challang.review.controller.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.*;
+import lombok.Builder;
+import lombok.Getter;
+import org.complete.challang.account.user.domain.entity.User;
+import org.complete.challang.drink.domain.entity.Drink;
+import org.complete.challang.review.controller.dto.item.SituationDto;
+import org.complete.challang.review.controller.dto.item.TasteDto;
+import org.complete.challang.review.domain.entity.Review;
+import org.complete.challang.review.domain.entity.ReviewFlavor;
+import org.complete.challang.review.domain.entity.ReviewFood;
+
+import java.util.List;
+
+@Getter
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ReviewCreateRequest {
+
+    @NotNull
+    private Long drinkId;
+
+    @NotBlank
+    private String imageUrl;
+
+    @NotBlank
+    @Size(min = 20, max = 250)
+    private String content;
+
+    @NotNull
+    @Min(0)
+    @Max(5)
+    private float rating;
+
+    private SituationDto situationDto;
+    private TasteDto tasteDto;
+    private List<Long> flavors;
+    private List<Long> foods;
+
+    public Review toEntity(final Drink drink,
+                           final User user,
+                           final List<ReviewFlavor> reviewFlavors,
+                           final List<ReviewFood> reviewFoods) {
+        return Review.builder()
+                .imageUrl(imageUrl)
+                .rating(rating)
+                .content(content)
+                .situation(situationDto.toEntity())
+                .taste(tasteDto.toEntity())
+                .drink(drink)
+                .user(user)
+                .reviewFlavors(reviewFlavors)
+                .reviewFoods(reviewFoods)
+                .build();
+    }
+}
