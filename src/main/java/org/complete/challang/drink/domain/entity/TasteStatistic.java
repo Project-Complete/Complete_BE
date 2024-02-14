5baf26564bc3894c39a4ce2aa5acb32fdfcdcf81
swoody1101@gmail.com <swoody1101@gmail.com>
2024-02-14 15:37:51 +0900

    feat: TasteStatistic 변수 업데이트 함수 추가
    - updateTasteRating 함수 추가

--- src/main/java/org/complete/challang/drink/domain/entity/TasteStatistic.java
+++ src/main/java/org/complete/challang/drink/domain/entity/TasteStatistic.java
@@ -1,7 +1,14 @@
 package org.complete.challang.drink.domain.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Embeddable
 public class TasteStatistic {
 
@@ -14,4 +21,16 @@ public class TasteStatistic {
     private double bodySumRating;
 
     private double refreshSumRating;
+
+    public void updateTasteRating(final double sweetRating,
+                                  final double sourRating,
+                                  final double bitterRating,
+                                  final double bodyRating,
+                                  double refreshRating) {
+        sweetSumRating += sweetRating;
+        sourSumRating += sourRating;
+        bitterSumRating += bitterRating;
+        bodySumRating += bodyRating;
+        refreshSumRating += refreshRating;
+    }
 }
