4b5fe77d2d6073f9ac6526b996c842897df39e0d
minoolian <mw.lee@kakao.com>
2024-02-14 14:58:45 +0900

    feat: Get 요청 인증처리
    - 임시로 Get 요청 permitall

--- src/main/java/org/complete/challang/config/SecurityConfig.java
+++ src/main/java/org/complete/challang/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.complete.challang.account.oauth2.handler.OAuth2SuccessHandler;
 import org.complete.challang.account.oauth2.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -42,6 +43,7 @@ public class SecurityConfig {
                 .formLogin(FormLoginConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(configurer ->
