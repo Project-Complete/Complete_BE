3326e52be0347065dbd296cc2018dd017077bfea
swoody1101@gmail.com <swoody1101@gmail.com>
2024-02-14 15:37:32 +0900

    feat: SituationStatistic 변수 업데이트 함수 추가
    - updateSituationStatistic 함수 추가

--- src/main/java/org/complete/challang/drink/domain/entity/SituationStatistic.java
+++ src/main/java/org/complete/challang/drink/domain/entity/SituationStatistic.java
@@ -1,7 +1,14 @@
 package org.complete.challang.drink.domain.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Embeddable
 public class SituationStatistic {
 
@@ -14,4 +21,26 @@ public class SituationStatistic {
     private long businessSum;
 
     private long aloneSum;
+
+    public void updateSituationStatistic(final boolean adult,
+                                         final boolean partner,
+                                         final boolean friend,
+                                         final boolean business,
+                                         final boolean alone) {
+        if (adult) {
+            adultSum++;
+        }
+        if (partner) {
+            partnerSum++;
+        }
+        if (friend) {
+            friendSum++;
+        }
+        if (business) {
+            businessSum++;
+        }
+        if (alone) {
+            aloneSum++;
+        }
+    }
 }
