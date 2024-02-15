92b1a60b66a9e857d1cb66e4ccbce7e1befd960b
swoody1101@gmail.com <swoody1101@gmail.com>
2024-02-15 22:14:09 +0900

    feat: REVIEW_NOT_FOUND 추가
    - 리뷰 정보가 존재하지 않을 경우 에러를 반환하기 위한 에러 코드 추가

--- src/main/java/org/complete/challang/common/exception/ErrorCode.java
+++ src/main/java/org/complete/challang/common/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     DRINK_NOT_FOUND("음료 정보가 존재하지 않습니다", HttpStatus.NOT_FOUND),
     FLAVOR_NOT_FOUND("향 정보가 존재하지 않습니다", HttpStatus.NOT_FOUND),
     FOOD_NOT_FOUND("음식 정보가 존재하지 않습니다", HttpStatus.NOT_FOUND),
+    REVIEW_NOT_FOUND("리뷰 정보가 존재하지 않습니다", HttpStatus.NOT_FOUND),
     USER_NOT_FOUND("유저 정보가 존재하지 않습니다", HttpStatus.NOT_FOUND);
 
     private final String message;
