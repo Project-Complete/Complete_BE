4da53652ebe4eb040e3f0b45d874840ce84bb2c8
swoody1101@gmail.com <swoody1101@gmail.com>
2024-02-15 20:56:35 +0900

    feat: findAll 함수 생성
    - 주류 리뷰의 리스트를 paging 처리하여 반환하는 함수 생성

--- src/main/java/org/complete/challang/review/domain/repository/ReviewRepository.java
+++ src/main/java/org/complete/challang/review/domain/repository/ReviewRepository.java
@@ -1,7 +1,11 @@
 package org.complete.challang.review.domain.repository;
 
 import org.complete.challang.review.domain.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findAll(Pageable pageable);
 }
