a703e992a7669b2c3532e3b05813b2ceba383ed1
minoolian <mw.lee@kakao.com>
2024-02-16 19:13:57 +0900

    refactor: PageInfoDto getter 명시
    - JSON 변환시 필요

--- src/main/java/org/complete/challang/common/dto/PageInfoDto.java
+++ src/main/java/org/complete/challang/common/dto/PageInfoDto.java
@@ -3,8 +3,10 @@ package org.complete.challang.common.dto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PageInfoDto {
 
