17d17238d9547eb977893b1830b95304638c3c82
swoody1101@gmail.com <swoody1101@gmail.com>
2024-02-17 23:42:32 +0900

    feat: updateTasteRating 함수 수정 및 downgradeTasteRating 함수 생성
    - updateTasteRating 함수 리펙토링
    - review 삭제 시 TasteStatistic의 필드 값을 다운그레이드 할 downgradeTasteRating 함수 생성

--- src/main/java/org/complete/challang/drink/domain/entity/TasteStatistic.java
+++ src/main/java/org/complete/challang/drink/domain/entity/TasteStatistic.java
@@ -2,6 +2,8 @@ package org.complete.challang.drink.domain.entity;
 
 import jakarta.persistence.Embeddable;
 import lombok.*;
+import org.complete.challang.review.controller.dto.item.TasteDto;
+import org.complete.challang.review.domain.entity.Taste;
 
 @Getter
 @Builder
@@ -20,15 +22,19 @@ public class TasteStatistic {
 
     private double refreshSumRating;
 
-    public void updateTasteRating(final double sweetRating,
-                                  final double sourRating,
-                                  final double bitterRating,
-                                  final double bodyRating,
-                                  double refreshRating) {
-        sweetSumRating += sweetRating;
-        sourSumRating += sourRating;
-        bitterSumRating += bitterRating;
-        bodySumRating += bodyRating;
-        refreshSumRating += refreshRating;
+    public void updateTasteRating(final TasteDto tasteDto) {
+        sweetSumRating += tasteDto.getSweet();
+        sourSumRating += tasteDto.getSour();
+        bitterSumRating += tasteDto.getBitter();
+        bodySumRating += tasteDto.getBody();
+        refreshSumRating += tasteDto.getRefresh();
+    }
+
+    public void downgradeTasteRating(final Taste taste) {
+        sweetSumRating -= taste.getSweet();
+        sourSumRating -= taste.getSour();
+        bitterSumRating -= taste.getBitter();
+        bodySumRating -= taste.getBody();
+        refreshSumRating -= taste.getRefresh();
     }
 }
