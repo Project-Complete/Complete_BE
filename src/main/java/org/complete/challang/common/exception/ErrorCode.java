48b4e2eb86100d85423f447b9493d3cf69ea9b84
minoolian <mw.lee@kakao.com>
2024-02-18 00:32:54 +0900

    feat: 평가별 주류 리스트 조회 서비스 로직 추가
    - 맛, 상황별 주류 리스트 조회 로직
    - 올바르지 않은 argument시 Errorcode

--- src/main/java/org/complete/challang/common/exception/ErrorCode.java
+++ src/main/java/org/complete/challang/common/exception/ErrorCode.java
@@ -12,7 +12,8 @@ public enum ErrorCode {
     FLAVOR_NOT_FOUND("향 정보가 존재하지 않습니다", HttpStatus.NOT_FOUND),
     FOOD_NOT_FOUND("음식 정보가 존재하지 않습니다", HttpStatus.NOT_FOUND),
     REVIEW_NOT_FOUND("리뷰 정보가 존재하지 않습니다", HttpStatus.NOT_FOUND),
-    USER_NOT_FOUND("유저 정보가 존재하지 않습니다", HttpStatus.NOT_FOUND);
+    USER_NOT_FOUND("유저 정보가 존재하지 않습니다", HttpStatus.NOT_FOUND),
+    DRINK_RATE_PARAM_INCORRECT("평가 조회 파라미터가 잘못되었습니다", HttpStatus.BAD_REQUEST);
 
     private final String message;
     private final HttpStatus httpStatus;
--- src/main/java/org/complete/challang/drink/service/DrinkService.java
+++ src/main/java/org/complete/challang/drink/service/DrinkService.java
@@ -3,11 +3,23 @@ package org.complete.challang.drink.service;
 import lombok.RequiredArgsConstructor;
 import org.complete.challang.common.exception.ApiException;
 import org.complete.challang.common.exception.ErrorCode;
-import org.complete.challang.drink.domain.entity.Drink;
 import org.complete.challang.drink.controller.dto.response.DrinkFindResponse;
+import org.complete.challang.drink.controller.dto.response.DrinkListFindResponse;
+import org.complete.challang.drink.controller.dto.response.DrinkPageResponse;
+import org.complete.challang.drink.domain.entity.Drink;
+import org.complete.challang.drink.domain.entity.SituationStatistic;
+import org.complete.challang.drink.domain.entity.TasteStatistic;
+import org.complete.challang.drink.domain.entity.criteria.DrinkSortCriteria;
+import org.complete.challang.drink.domain.entity.spec.DrinkSpec;
 import org.complete.challang.drink.domain.repository.DrinkRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -18,10 +30,54 @@ public class DrinkService {
 
     @Transactional(readOnly = true)
     public DrinkFindResponse findDetailDrink(Long drinkId) {
-        Drink drink = drinkRepository.findById(drinkId).orElseThrow(() -> new ApiException(ErrorCode.DRINK_NOT_FOUND));
+        Drink drink = findDrink(drinkId);
         DrinkFindResponse drinkFindResponse = drink.toDto();
         drinkFindResponse.updateStatistic(drinkRepository.findFoodStatisticById(drinkId), drinkRepository.findFlavorStatisticById(drinkId));
 
         return drinkFindResponse;
+    }
+
+    @Transactional(readOnly = true)
+    public DrinkPageResponse<DrinkListFindResponse> findRateDrinks(final Long drinkId,
+                                                                   final String rate) {
+        final Drink drink = findDrink(drinkId);
+
+        String maxField = null;
+        if (rate.equals("taste")) {
+            final TasteStatistic tasteStatistic = drink.getTasteStatistic();
+            final Field[] tasteFields = TasteStatistic.class.getDeclaredFields();
+            maxField = extractMaxRateField(tasteStatistic, tasteFields);
+        } else if (rate.equals("situation")) {
+            final SituationStatistic situationStatistic = drink.getSituationStatistic();
+            final Field[] situationFields = SituationStatistic.class.getDeclaredFields();
+            maxField = extractMaxRateField(situationStatistic, situationFields);
+        }
+
+        if (maxField == null) throw new ApiException(ErrorCode.DRINK_RATE_PARAM_INCORRECT);
+
+        DrinkSortCriteria drinkSortCriteria = DrinkSortCriteria.getDrinkSortCriteria(maxField);
+        final Page<Drink> drinks = drinkRepository.findAll(DrinkSpec.orderByRate(drinkSortCriteria), PageRequest.of(0, 4));
+        final List<DrinkListFindResponse> drinkListFindResponses = drinks.getContent().stream().map(DrinkListFindResponse::toDto).toList();
+
+        return DrinkPageResponse.toDto(drinkListFindResponses, drinks, maxField);
+    }
+
+    private Drink findDrink(final Long drinkId) {
+        return drinkRepository.findById(drinkId).orElseThrow(() -> new ApiException(ErrorCode.DRINK_NOT_FOUND));
+    }
+
+    private <T> String extractMaxRateField(final T statistic,
+                                           Field[] fields) {
+        Arrays.sort(fields, (o1, o2) -> {
+            try {
+                o1.setAccessible(true);
+                o2.setAccessible(true);
+                return Double.compare(o2.getDouble(statistic), o1.getDouble(statistic));
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException("존재하지 않는 Field입니다");
+            }
+        });
+
+        return fields[0].getName();
     }
 }
