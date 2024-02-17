4be2e0df364e5b50fcc1511bfa85986bb5cab005
swoody1101@gmail.com <swoody1101@gmail.com>
2024-02-17 23:28:19 +0900

    feat: findAllByIsActiveTrue, findByIdAndIsActiveTrue, findByIdAndUserIdAndIsActiveTrue 함수 생성
    - 기존의 findAll 함수를 삭제된 리뷰를 제외한 리뷰 항목들을 불러우기 위한 findAllByIsActiveTrue 함수로 변경
    - 상세 리뷰를 보기 위한 findByIdAndIsActiveTrue 함수 생성
    - 리뷰의 삭제 권한을 보기 위한 findByIdAndUserIdAndIsActiveTrue 함수 생성

--- src/main/java/org/complete/challang/review/domain/repository/ReviewRepository.java
+++ src/main/java/org/complete/challang/review/domain/repository/ReviewRepository.java
@@ -5,7 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    Page<Review> findAll(Pageable pageable);
+    Page<Review> findAllByIsActiveTrue(Pageable pageable);
+
+    Optional<Review> findByIdAndIsActiveTrue(final Long reviewId);
+
+    Optional<Review> findByIdAndUserIdAndIsActiveTrue(final Long reviewId, final Long userId);
 }
