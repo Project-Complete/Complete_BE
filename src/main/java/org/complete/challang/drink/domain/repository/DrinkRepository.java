f087c2b34281b7e7899b08145e4288fd55a7bda6
swoody1101@gmail.com <swoody1101@gmail.com>
2024-02-17 23:22:57 +0900

    feat: findByIdAndIsActiveTrue
    - 삭제된 주류를 제외한 음료 항목을 찾기위한 함수 생성

--- src/main/java/org/complete/challang/drink/domain/repository/DrinkRepository.java
+++ src/main/java/org/complete/challang/drink/domain/repository/DrinkRepository.java
@@ -3,5 +3,9 @@ package org.complete.challang.drink.domain.repository;
 import org.complete.challang.drink.domain.entity.Drink;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DrinkRepository extends JpaRepository<Drink, Long>, DrinkQueryRepository {
+
+    Optional<Drink> findByIdAndIsActiveTrue(Long drinkId);
 }
