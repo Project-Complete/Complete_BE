a5b935b1bf732ae8e68ae654845a3fa575fd0caa
minoolian <mw.lee@kakao.com>
2024-02-19 04:48:37 +0900

    feat: 특정 Flavor 기준 주류 리스트 조회
    - 특정 Flavor가 리뷰에서 많이 언급된 순으로 주류 리스트 조회
    - JPQL 문법으로 작성

--- src/main/java/org/complete/challang/drink/domain/repository/DrinkRepository.java
+++ src/main/java/org/complete/challang/drink/domain/repository/DrinkRepository.java
@@ -1,13 +1,29 @@
 package org.complete.challang.drink.domain.repository;
 
+import org.complete.challang.drink.controller.dto.response.DrinkListFindResponse;
 import org.complete.challang.drink.domain.entity.Drink;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DrinkRepository extends JpaRepository<Drink, Long>, DrinkQueryRepository, JpaSpecificationExecutor<Drink> {
 
     Page<Drink> findAll(Specification<Drink> spec, Pageable pageable);
+
+    @Query(value = "select new org.complete.challang.drink.controller.dto.response.DrinkListFindResponse(d.id, d.imageUrl, d.drinkManufacturer.manufacturerName, d.name, d.reviewSumRating/d.reviewCount) "
+            + "from Drink d "
+            + "left join d.reviews r "
+            + "left join r.reviewFlavors rf "
+            + "left join rf.flavor f "
+            + "on f.flavor = :flavor "
+            + "where d.id != :drinkId "
+            + "group by d.id "
+            + "order by count(f.flavor) desc")
+    Page<DrinkListFindResponse> findDrinksOrderByMaxFlavor(@Param("flavor") final String flavor,
+                                                           @Param("drinkId") final Long drinkId,
+                                                           final Pageable pageable);
 }
