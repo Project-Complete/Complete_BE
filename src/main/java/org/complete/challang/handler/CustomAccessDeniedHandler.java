c02a567e06fa0ca55c936d5d6248fea56125a1a4
minoolian <mw.lee@kakao.com>
2024-02-19 19:15:30 +0900

    feature: 인증 및 인가 custom handler 구현
    - AuthenticationEntryPoint custom 구현
    - AccessDeniedHandler custom 구현
    - Security Config에 구현된 handler 적

---
+++ src/main/java/org/complete/challang/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,37 @@
+package org.complete.challang.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.complete.challang.common.exception.ApiException;
+import org.complete.challang.common.exception.ErrorCode;
+import org.complete.challang.common.exception.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        ErrorResponse errorResponse = ErrorResponse.toErrorResponse(new ApiException(ErrorCode.FORBIDDEN));
+        String responseBody = objectMapper.writeValueAsString(errorResponse);
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(responseBody);
+    }
+}
