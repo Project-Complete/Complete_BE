89719e605890b42a441c0a2e242b8c569e5d5d3d
swoody1101@gmail.com <swoody1101@gmail.com>
2024-02-19 17:22:58 +0900

    feat: UserProfileFindResponse DTO 생성
    - 사용자의 프로필정보를 반환하기 위한 DTO class 및 toDto 함수 새성

---
+++ src/main/java/org/complete/challang/account/user/controller/dto/response/UserProfileFindResponse.java
@@ -0,0 +1,30 @@
+package org.complete.challang.account.user.controller.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Getter;
+import org.complete.challang.account.user.domain.entity.User;
+
+@Getter
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class UserProfileFindResponse {
+
+    private String profileImageUrl;
+    private String nickname;
+    private String email;
+    private Long followers;
+    private Long followings;
+
+    public static UserProfileFindResponse toDto(final User user,
+                                                final String email) {
+        return UserProfileFindResponse.builder()
+                .profileImageUrl(user.getProfileImageUrl())
+                .nickname(user.getNickname())
+                .email(email)
+                .followers(user.getFollowers())
+                .followings(user.getFollowings())
+                .build();
+    }
+}
