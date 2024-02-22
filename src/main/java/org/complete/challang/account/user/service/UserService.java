cd251f393b2265059c29e3343001431b472e882e
swoody1101@gmail.com <swoody1101@gmail.com>
2024-02-22 16:51:03 +0900

    feat: createFollow 함수 생성
    - 사용자를 찾는 중복된 로직을 findUserById 함수를 생성하여 변경
    - createFollow 함수를 생성하여 follow 기능을 추가하며, 사용자가 자신을 팔로우 할 경우 그리고 이미 팔로잉 한 상태을 경우 예외처리

--- src/main/java/org/complete/challang/account/user/service/UserService.java
+++ src/main/java/org/complete/challang/account/user/service/UserService.java
@@ -4,31 +4,34 @@ import lombok.RequiredArgsConstructor;
 import org.complete.challang.account.user.controller.dto.request.ProfileUpdateRequest;
 import org.complete.challang.account.user.controller.dto.response.ProfileUpdateResponse;
 import org.complete.challang.account.user.controller.dto.response.UserProfileFindResponse;
+import org.complete.challang.account.user.domain.entity.Follow;
 import org.complete.challang.account.user.domain.entity.User;
+import org.complete.challang.account.user.domain.repository.FollowRepository;
 import org.complete.challang.account.user.domain.repository.UserRepository;
 import org.complete.challang.common.exception.ApiException;
+import org.complete.challang.common.exception.SuccessCode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.complete.challang.common.exception.ErrorCode.*;
+import static org.complete.challang.common.exception.SuccessCode.FOLLOW_CREATE_SUCCESS;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
+    private final FollowRepository followRepository;
     private final UserRepository userRepository;
 
     public UserProfileFindResponse findUserProfile(final Long myId,
                                                    final Long userId) {
         User user;
         if (userId == null) {
-            user = userRepository.findById(myId)
-                    .orElseThrow(() -> new ApiException(USER_NOT_FOUND));
+            user = findUserById(myId);
 
             return UserProfileFindResponse.toDto(user, user.getEmail());
         } else {
-            user = userRepository.findById(userId)
-                    .orElseThrow(() -> new ApiException(USER_NOT_FOUND));
+            user = findUserById(userId);
 
             return UserProfileFindResponse.toDto(user, null);
         }
@@ -37,8 +40,7 @@ public class UserService {
     @Transactional
     public ProfileUpdateResponse updateProfile(final Long userId,
                                                final ProfileUpdateRequest profileUpdateRequest) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new ApiException(USER_NOT_FOUND));
+        User user = findUserById(userId);
 
         user.updateProfileImageUrl(profileUpdateRequest.getProfileImageUrl());
         updateEmail(user, profileUpdateRequest.getEmail());
@@ -47,13 +49,39 @@ public class UserService {
         return ProfileUpdateResponse.toDto(user);
     }
 
+    @Transactional
+    public SuccessCode createFollow(final Long userId,
+                                    final Long followId) {
+        User fromUser = findUserById(userId);
+        User toUSer = findUserById(followId);
+        if (fromUser == toUSer) {
+            throw new ApiException(INVALID_FOLLOW_REQUEST);
+        }
+        if (followRepository.existsByFromUserIdAndToUserId(fromUser.getId(), toUSer.getId())) {
+            throw new ApiException(FOLLOW_CONFLICT);
+        }
+
+        Follow follow = Follow.builder()
+                .fromUser(fromUser)
+                .toUser(toUSer)
+                .build();
+        followRepository.save(follow);
+
+        return FOLLOW_CREATE_SUCCESS;
+    }
+
+    private User findUserById(final Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new ApiException(USER_NOT_FOUND));
+    }
+
     private void updateEmail(final User user,
                              final String newEmail) {
         if (user.getEmail() == newEmail) {
             return;
         }
         if (userRepository.existsByEmail(newEmail)) {
-            new ApiException(EMAIL_CONFLICT);
+            throw new ApiException(EMAIL_CONFLICT);
         }
         user.updateEmail(newEmail);
     }
@@ -64,7 +92,7 @@ public class UserService {
             return;
         }
         if (userRepository.existsByNickname(newNickname)) {
-            new ApiException(NICKNAME_CONFLICT);
+            throw new ApiException(NICKNAME_CONFLICT);
         }
         user.updateNickname(newNickname);
     }
