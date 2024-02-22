b7b209d2cbc3dbc9e90dde75f6d2d15f4371e77b
swoody1101@gmail.com <swoody1101@gmail.com>
2024-02-22 16:10:30 +0900

    feat: Follow entity 생성
    - 팔로우 기능 구현을 위해 User entity와 N:1로 매핑된 Follow entity 생성

---
+++ src/main/java/org/complete/challang/account/user/domain/entity/Follow.java
@@ -0,0 +1,24 @@
+package org.complete.challang.account.user.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.complete.challang.common.domain.entity.BaseEntity;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@AttributeOverride(name = "id", column = @Column(name = "follow_id"))
+@Entity
+public class Follow extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User toUser;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User fromUser;
+}
