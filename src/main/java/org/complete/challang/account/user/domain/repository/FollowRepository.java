c61b44fd4145b1825a264e06da2661e7e2315fa8
swoody1101@gmail.com <swoody1101@gmail.com>
2024-02-24 20:32:49 +0900

    feat: deleteByFromUserIdAndToUserID 생성
    - 특정 팔로우를 취소하기 위한 함수 생성

--- src/main/java/org/complete/challang/account/user/domain/repository/FollowRepository.java
+++ src/main/java/org/complete/challang/account/user/domain/repository/FollowRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
     boolean existsByFromUserIdAndToUserId(final Long fromUserId, final Long toUserId);
+
+    void deleteByFromUserIdAndToUserID(final Long fromUserId, final Long toUserId);
 }
