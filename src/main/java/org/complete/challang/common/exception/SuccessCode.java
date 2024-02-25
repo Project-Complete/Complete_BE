3ba60318ab79146c2872132937baa64e96de4e09
minoolian <mw.lee@kakao.com>
2024-02-26 01:30:02 +0900

    feat: 주류 좋아요 관련 ErrorCode 및 SuccessCode 추가
    - 주류 좋아요 not found 및 conflict 에러
    - 주류 좋아요 추가, 삭제 성공 메시지

--- src/main/java/org/complete/challang/common/exception/SuccessCode.java
+++ src/main/java/org/complete/challang/common/exception/SuccessCode.java
@@ -9,9 +9,11 @@ import org.springframework.http.HttpStatus;
 public enum SuccessCode {
 
     FOLLOW_CREATE_SUCCESS("팔로우 생성을 성공했습니다.", HttpStatus.CREATED),
+    DRINK_LIKE_SUCCESS("주류 좋아요를 성공했습니다", HttpStatus.CREATED),
 
     FOLLOW_DELETE_SUCCESS("팔로우 삭제를 성공했습니다.", HttpStatus.NO_CONTENT),
     REVIEW_DELETE_SUCCESS("리뷰 삭제를 성공했습니다", HttpStatus.NO_CONTENT),
+    DRINK_LIKE_DELETE_SUCCESS("주류 좋아요 삭제를 성공했습니다", HttpStatus.NO_CONTENT)
     ;
 
     private final String message;
