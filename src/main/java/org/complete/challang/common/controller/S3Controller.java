091a31a0c594e12227f8a284f4a964c50bc11cde
minoolian <mw.lee@kakao.com>
2024-02-27 23:58:13 +0900

    chore: Drink 및 S3 Controller Swagger 설정

--- src/main/java/org/complete/challang/common/controller/S3Controller.java
+++ src/main/java/org/complete/challang/common/controller/S3Controller.java
@@ -1,5 +1,7 @@
 package org.complete.challang.common.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.complete.challang.common.controller.dto.request.PreSignedUrlGetRequest;
 import org.complete.challang.common.controller.dto.response.PreSignedUrlGetResponse;
@@ -8,14 +10,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Pre-Signed URL API", description = "S3 pre-signed url 요청 API")
 @RestController
 @RequiredArgsConstructor
 public class S3Controller {
 
     private final S3Service s3Service;
 
+    @Operation(summary = "Pre-Signed URL 요청", description = "pre-signed url 반환 요청")
     @PostMapping("/pre-signed-url")
     public ResponseEntity<PreSignedUrlGetResponse> getPreSignedUrl(@RequestBody PreSignedUrlGetRequest preSignedUrlGetRequest,
                                                                    @AuthenticationPrincipal UserDetails userDetails) {
--- src/main/java/org/complete/challang/drink/controller/DrinkController.java
+++ src/main/java/org/complete/challang/drink/controller/DrinkController.java
@@ -1,5 +1,7 @@
 package org.complete.challang.drink.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.complete.challang.common.exception.SuccessResponse;
 import org.complete.challang.drink.controller.dto.response.DrinkFindResponse;
@@ -12,6 +14,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "주류 API", description = "주류 관련 API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/drink")
@@ -19,12 +22,14 @@ public class DrinkController {
 
     private final DrinkService drinkService;
 
+    @Operation(summary = "주류 상세 조회", description = "주류 단건 상세 조회")
     @GetMapping("/detail/{drink_id}")
     public ResponseEntity<DrinkFindResponse> findDetailDrink(@PathVariable("drink_id") Long drinkId,
                                                              @AuthenticationPrincipal UserDetails userDetails) {
         return new ResponseEntity<>(drinkService.findDetailDrink(drinkId, userDetails), HttpStatus.OK);
     }
 
+    @Operation(summary = "주류 평가 리스트 조회", description = "주류 평가(맛/향/상황)별 추천 리스트 조회")
     @GetMapping("/{drink_id}/search")
     public ResponseEntity<DrinkPageResponse<DrinkListFindResponse>> findRateDrinks(@PathVariable("drink_id") Long drinkId,
                                                                                    @RequestParam("rate") String rate,
@@ -32,6 +37,7 @@ public class DrinkController {
         return new ResponseEntity<>(drinkService.findRateDrinks(drinkId, rate, userDetails), HttpStatus.OK);
     }
 
+    @Operation(summary = "주류 타입별 리스트 조회", description = "주류 타입(맥주/전통주)별 및 정렬순 리스트 조회")
     @GetMapping("/search")
     public ResponseEntity<DrinkPageResponse<DrinkListFindResponse>> findDrinks(@RequestParam("drink_type") String drinkType,
                                                                                @RequestParam("sorted") String sorted,
@@ -40,12 +46,14 @@ public class DrinkController {
         return new ResponseEntity<>(drinkService.findDrinks(drinkType, sorted, page, userDetails), HttpStatus.OK);
     }
 
+    @Operation(summary = "주류 좋아요", description = "유저 주류 좋아요 추가")
     @PostMapping("/{drink_id}/like")
     public ResponseEntity<SuccessResponse> createLikeDrink(@PathVariable("drink_id") Long drinkId,
                                                            @AuthenticationPrincipal UserDetails userDetails) {
         return new ResponseEntity<>(drinkService.likeDrink(drinkId, userDetails), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "주류 좋아요 삭제", description = "유저 주류 좋아요 삭제")
     @DeleteMapping("/{drink_id}/like")
     public ResponseEntity<SuccessResponse> deleteLikeDrink(@PathVariable("drink_id") Long drinkId,
                                                            @AuthenticationPrincipal UserDetails userDetails) {
