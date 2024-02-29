e756b6d2e08d850851c2f30895865de24ed37b23
minoolian <mw.lee@kakao.com>
2024-02-29 17:24:30 +0900

    feat: 주류 추가 관련 Entity 로직

--- src/main/java/org/complete/challang/app/drink/domain/entity/Drink.java
+++ src/main/java/org/complete/challang/app/drink/domain/entity/Drink.java
@@ -3,19 +3,15 @@ package org.complete.challang.app.drink.domain.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
-import org.complete.challang.app.account.user.domain.entity.DrinkBookmark;
 import org.complete.challang.app.account.user.domain.entity.DrinkLike;
 import org.complete.challang.app.account.user.domain.entity.User;
 import org.complete.challang.app.common.domain.entity.BaseEntity;
 import org.complete.challang.app.common.exception.ApiException;
 import org.complete.challang.app.common.exception.ErrorCode;
-import org.complete.challang.app.drink.controller.dto.item.*;
-import org.complete.challang.app.drink.controller.dto.response.*;
 import org.complete.challang.app.review.domain.entity.Review;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @SuperBuilder
@@ -42,14 +38,17 @@ public class Drink extends BaseEntity {
 
     private String title;
 
+    @Builder.Default
     @Embedded
-    private FoodStatistic foodStatistic;
+    private FoodStatistic foodStatistic = new FoodStatistic();
 
+    @Builder.Default
     @Embedded
-    private SituationStatistic situationStatistic;
+    private SituationStatistic situationStatistic = new SituationStatistic();
 
+    @Builder.Default
     @Embedded
-    private TasteStatistic tasteStatistic;
+    private TasteStatistic tasteStatistic = new TasteStatistic();
 
     @ManyToOne(fetch = FetchType.LAZY)
     private DrinkDetailType drinkDetailType;
@@ -62,7 +61,7 @@ public class Drink extends BaseEntity {
     private List<DrinkLike> drinkLikes = new ArrayList<>();
 
     @Builder.Default
-    @OneToMany(mappedBy = "drink", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "drink", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<DrinkPackage> drinkPackages = new ArrayList<>();
 
     @Builder.Default
@@ -109,5 +108,26 @@ public class Drink extends BaseEntity {
         }
 
         drinkLikes.remove(drinkLike);
+    }
+
+    public void updateDrinkManufacturer(final DrinkManufacturer drinkManufacturer) {
+        this.drinkManufacturer = drinkManufacturer;
+    }
+
+    public void updateDrinkDetailType(final DrinkDetailType drinkDetailType) {
+        this.drinkDetailType = drinkDetailType;
+    }
+
+    public void addDrinkPackages(final Package packages) {
+        final DrinkPackage drinkPackage = DrinkPackage.builder()
+                .drink(this)
+                .packages(packages)
+                .build();
+
+        if (drinkPackages.contains(drinkPackage)) {
+            throw new ApiException(ErrorCode.DRINK_PACKAGE_CONFLICT);
+        }
+
+        drinkPackages.add(drinkPackage);
     }
 }
