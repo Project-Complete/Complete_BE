041a12271e04ab6ecc8c2568a92e4a6d452ee853
minoolian <mw.lee@kakao.com>
2024-05-12 22:47:28 +0900

    chore: AOP 및 Discord appender 의존성 추가
    - Logging Aspect 사용을 위한 AOP 의존성
    - Discord Webhook error 전송을 위한 의존

--- src/main/java/org/complete/challang/ChallangApplication.java
+++ src/main/java/org/complete/challang/ChallangApplication.java
@@ -2,10 +2,12 @@ package org.complete.challang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAspectJAutoProxy
 public class ChallangApplication {
 
     public static void main(String[] args) {
