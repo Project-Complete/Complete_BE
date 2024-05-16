f0b482ee03cdb823d4bd45a5851251ba031c3c10
minoolian <mw.lee@kakao.com>
2024-05-16 19:02:25 +0900

    refactor: 주류 Controller 유효성 검사 구성
    - @IdValid 및 @Valid 어노테이션을 통한 유효성 검증

--- src/main/java/org/complete/challang/app/drink/controller/DrinkController.java
+++ src/main/java/org/complete/challang/app/drink/controller/DrinkController.java
@@ -2,8 +2,11 @@ package org.complete.challang.app.drink.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.complete.challang.annotation.AuthUser;
+import org.complete.challang.annotation.IdValid;
 import org.complete.challang.app.account.oauth2.CustomOAuth2User;
 import org.complete.challang.app.common.exception.SuccessResponse;
 import org.complete.challang.app.drink.controller.dto.request.DrinkCreateRequest;
@@ -11,10 +14,12 @@ import org.complete.challang.app.drink.controller.dto.response.*;
 import org.complete.challang.app.drink.service.DrinkService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Drink", description = "주류 관련 API")
 @RequiredArgsConstructor
+@Validated
 @RequestMapping("/drinks")
 @RestController
 public class DrinkController {
@@ -23,7 +28,8 @@ public class DrinkController {
 
     @Operation(summary = "주류 상세 조회", description = "주류 단건 상세 조회")
     @GetMapping("/detail/{drink_id}")
-    public ResponseEntity<DrinkFindResponse> findDetailDrink(@PathVariable("drink_id") final Long drinkId,
+    public ResponseEntity<DrinkFindResponse> findDetailDrink(@PathVariable("drink_id")
+                                                             @IdValid final Long drinkId,
                                                              @AuthUser final CustomOAuth2User customOAuth2User) {
 
         return new ResponseEntity<>(drinkService.findDetailDrink(drinkId, customOAuth2User), HttpStatus.OK);
@@ -31,7 +37,8 @@ public class DrinkController {
 
     @Operation(summary = "주류 평가 리스트 조회", description = "주류 평가(맛/향/상황)별 추천 리스트 조회")
     @GetMapping("/{drink_id}/search")
-    public ResponseEntity<DrinkPageResponse<DrinkListFindResponse>> findRateDrinks(@PathVariable("drink_id") final Long drinkId,
+    public ResponseEntity<DrinkPageResponse<DrinkListFindResponse>> findRateDrinks(@PathVariable("drink_id")
+                                                                                   @IdValid final Long drinkId,
                                                                                    @RequestParam("rate") final String rate,
                                                                                    @AuthUser final CustomOAuth2User customOAuth2User) {
 
@@ -42,7 +49,8 @@ public class DrinkController {
     @GetMapping("/search")
     public ResponseEntity<DrinkPageResponse<DrinkListFindResponse>> findDrinks(@RequestParam("drink_type") final String drinkType,
                                                                                @RequestParam("sorted") final String sorted,
-                                                                               @RequestParam(value = "page", defaultValue = "1") final int page,
+                                                                               @RequestParam(value = "page", defaultValue = "1")
+                                                                               @Positive(message = "page는 1이상이어야 합니다") final int page,
                                                                                @AuthUser final CustomOAuth2User customOAuth2User) {
 
         return new ResponseEntity<>(drinkService.findDrinks(drinkType, sorted, page, customOAuth2User), HttpStatus.OK);
@@ -50,7 +58,8 @@ public class DrinkController {
 
     @Operation(summary = "주류 좋아요", description = "유저 주류 좋아요 추가")
     @PostMapping("/{drink_id}/like")
-    public ResponseEntity<SuccessResponse> createLikeDrink(@PathVariable("drink_id") final Long drinkId,
+    public ResponseEntity<SuccessResponse> createLikeDrink(@PathVariable("drink_id")
+                                                           @IdValid final Long drinkId,
                                                            @AuthUser final CustomOAuth2User customOAuth2User) {
 
         return new ResponseEntity<>(drinkService.likeDrink(drinkId, customOAuth2User), HttpStatus.CREATED);
@@ -58,7 +67,8 @@ public class DrinkController {
 
     @Operation(summary = "주류 좋아요 삭제", description = "유저 주류 좋아요 삭제")
     @DeleteMapping("/{drink_id}/like")
-    public ResponseEntity<SuccessResponse> deleteLikeDrink(@PathVariable("drink_id") final Long drinkId,
+    public ResponseEntity<SuccessResponse> deleteLikeDrink(@PathVariable("drink_id")
+                                                           @IdValid final Long drinkId,
                                                            @AuthUser final CustomOAuth2User customOAuth2User) {
 
         return new ResponseEntity<>(drinkService.unLikeDrink(drinkId, customOAuth2User), HttpStatus.OK);
@@ -73,7 +83,8 @@ public class DrinkController {
 
     @Operation(summary = "주류 추가", description = "주류 추가 (관리자 기능)")
     @PostMapping
-    public ResponseEntity<DrinkCreateResponse> createDrink(@RequestBody final DrinkCreateRequest drinkCreateRequest) {
+    public ResponseEntity<DrinkCreateResponse> createDrink(@RequestBody
+                                                           @Valid final DrinkCreateRequest drinkCreateRequest) {
 
         return new ResponseEntity<>(drinkService.createDrink(drinkCreateRequest), HttpStatus.OK);
     }
