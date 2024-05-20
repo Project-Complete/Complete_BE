84f67ec1469934a775facf32b50fccc181e7f0f3
minoolian <mw.lee@kakao.com>
2024-05-20 18:55:14 +0900

    feat: 주류 조합 작성 DTO 추가
    - CombinationBoardCreateRequest 주류 조합 게시글 작성 DTO
    - CombinationCreateDto 주류 조합 모음 DTO

---
+++ src/main/java/org/complete/challang/app/combination/controller/dto/item/CombinationCreateDto.java
@@ -0,0 +1,36 @@
+package org.complete.challang.app.combination.controller.dto.item;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import org.complete.challang.app.combination.domain.entity.Combination;
+import org.complete.challang.app.combination.domain.entity.CombinationBoard;
+import org.complete.challang.app.drink.domain.entity.Drink;
+
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class CombinationCreateDto {
+
+    private Long drinkId;
+    private String name;
+    private String volume;
+
+    @JsonProperty("x_coordinate")
+    private int xCoordinate;
+
+    @JsonProperty("y_coordinate")
+    private int yCoordinate;
+
+    public Combination toEntity(CombinationBoard combinationBoard,
+                                Drink drink) {
+        return Combination.builder()
+                .name(drink == null ? this.name : drink.getName())
+                .volume(this.volume)
+                .xCoordinate(this.xCoordinate)
+                .yCoordinate(this.yCoordinate)
+                .combinationBoard(combinationBoard)
+                .drink(drink)
+                .build();
+    }
+}
