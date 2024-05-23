a655d327eaa476b70cca9663653d3eac118ece71
minoolian <mw.lee@kakao.com>
2024-05-23 18:31:49 +0900

    feat: 주류 조합 리스트 조회 서비스 로직 추가
    - 앞서 작성한 QueryDSL 및 CombinationSortCriteria를 이용하여 코드 구성

--- src/main/java/org/complete/challang/app/combination/service/CombinationService.java
+++ src/main/java/org/complete/challang/app/combination/service/CombinationService.java
@@ -6,13 +6,18 @@ import org.complete.challang.app.account.user.domain.repository.UserRepository;
 import org.complete.challang.app.combination.controller.dto.request.CombinationBoardCreateRequest;
 import org.complete.challang.app.combination.controller.dto.response.CombinationBoardCreateResponse;
 import org.complete.challang.app.combination.controller.dto.response.CombinationBoardFindResponse;
+import org.complete.challang.app.combination.controller.dto.response.CombinationBoardListFindResponse;
+import org.complete.challang.app.combination.controller.dto.response.CombinationBoardPageResponse;
 import org.complete.challang.app.combination.domain.entity.Combination;
 import org.complete.challang.app.combination.domain.entity.CombinationBoard;
-import org.complete.challang.app.combination.repository.CombinationBoardRepository;
+import org.complete.challang.app.combination.domain.entity.CombinationSortCriteria;
+import org.complete.challang.app.combination.domain.repository.CombinationBoardRepository;
 import org.complete.challang.app.common.exception.ApiException;
 import org.complete.challang.app.common.exception.ErrorCode;
 import org.complete.challang.app.drink.domain.entity.Drink;
 import org.complete.challang.app.drink.domain.repository.DrinkRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,6 +37,19 @@ public class CombinationService {
         final CombinationBoard combinationBoard = combinationBoardRepository.findById(combinationBoardId).orElseThrow(() -> new ApiException(ErrorCode.COMBINATION_BOARD_NOT_FOUND));
 
         return CombinationBoardFindResponse.toDto(combinationBoard, userId);
+    }
+
+    public CombinationBoardPageResponse<CombinationBoardListFindResponse> findCombinationBoards(final int page,
+                                                                                                final String sorted,
+                                                                                                final Long userId) {
+        CombinationSortCriteria combinationSortCriteria = CombinationSortCriteria.getCombinationSortCriteria(sorted);
+        Page<CombinationBoardListFindResponse> combinations = combinationBoardRepository.findAllBySorted(
+                combinationSortCriteria,
+                PageRequest.of(page - 1, 8),
+                userId
+        );
+
+        return CombinationBoardPageResponse.toDto(combinations.getContent(), combinations, combinationSortCriteria.getDescription());
     }
 
     @Transactional
