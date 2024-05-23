1b1f3428f6a949a1d3aa5e26d38ce091cbc16f6a
minoolian <mw.lee@kakao.com>
2024-05-23 20:01:51 +0900

    feat: 주류조합 수정 Entity 로직 추가
    - Combination Entity HashCode & Equals override
    - CombinationBoard Entity 내 update 로직 추가

--- src/main/java/org/complete/challang/app/combination/domain/entity/CombinationBoard.java
+++ src/main/java/org/complete/challang/app/combination/domain/entity/CombinationBoard.java
@@ -6,14 +6,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.complete.challang.app.account.user.domain.entity.User;
+import org.complete.challang.app.combination.controller.dto.request.CombinationBoardUpdateRequest;
 import org.complete.challang.app.common.domain.entity.BaseEntity;
+import org.hibernate.annotations.DynamicUpdate;
 
-import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Getter
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AttributeOverride(name = "id", column = @Column(name = "combination_board_id"))
+@DynamicUpdate
 @Entity
 public class CombinationBoard extends BaseEntity {
 
@@ -29,9 +33,24 @@ public class CombinationBoard extends BaseEntity {
     private User user;
 
     @OneToMany(mappedBy = "combinationBoard", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Combination> combinations;
+    private Set<Combination> combinations;
 
-    public void updateCombinations(final List<Combination> combinations) {
-        this.combinations = combinations;
+    public void updateCombinations(final Set<Combination> combinations) {
+        if (this.combinations.size() == combinations.size() && this.combinations.equals(combinations)) {
+            return;
+        }
+        this.combinations.clear();
+        this.combinations.addAll(combinations);
+    }
+
+    public void updateCombinationBoard(CombinationBoardUpdateRequest combinationBoardUpdateRequest) {
+        Optional.ofNullable(combinationBoardUpdateRequest.getTitle())
+                .ifPresent(title -> this.title = title);
+        Optional.ofNullable(combinationBoardUpdateRequest.getContent())
+                .ifPresent(content -> this.content = content);
+        Optional.ofNullable(combinationBoardUpdateRequest.getImageUrl())
+                .ifPresent(imageUrl -> this.imageUrl = imageUrl);
+        Optional.ofNullable(combinationBoardUpdateRequest.getDescription())
+                .ifPresent(description -> this.description = description);
     }
 }
