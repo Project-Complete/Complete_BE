bd7b61d416c211c5efc422736d9bde1ebfa01c5d
minoolian <mw.lee@kakao.com>
2024-05-23 18:32:17 +0900

    feat: 주류 조합 리스트 조회 API 추가

--- src/main/java/org/complete/challang/app/combination/controller/CombinationController.java
+++ src/main/java/org/complete/challang/app/combination/controller/CombinationController.java
@@ -2,16 +2,16 @@ package org.complete.challang.app.combination.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.complete.challang.annotation.AuthUser;
 import org.complete.challang.app.account.oauth2.CustomOAuth2User;
-import org.complete.challang.app.combination.controller.dto.item.CombinationCreateDto;
 import org.complete.challang.app.combination.controller.dto.request.CombinationBoardCreateRequest;
 import org.complete.challang.app.combination.controller.dto.response.CombinationBoardCreateResponse;
 import org.complete.challang.app.combination.controller.dto.response.CombinationBoardFindResponse;
+import org.complete.challang.app.combination.controller.dto.response.CombinationBoardListFindResponse;
+import org.complete.challang.app.combination.controller.dto.response.CombinationBoardPageResponse;
 import org.complete.challang.app.combination.service.CombinationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,15 +35,21 @@ public class CombinationController {
         return new ResponseEntity<>(combinationService.findCombinationBoard(combinationBoardId, userId), HttpStatus.OK);
     }
 
+    @Operation(summary = "주류 조합 리스트 조회", description = "주류 조합 목록 조회")
+    @GetMapping("/search")
+    public ResponseEntity<CombinationBoardPageResponse<CombinationBoardListFindResponse>> findCombinationBoards(@RequestParam(value = "page", defaultValue = "1") final int page,
+                                                                                                                @RequestParam("sorted") final String sorted,
+                                                                                                                @AuthUser final CustomOAuth2User customOAuth2User) {
+        Long userId = customOAuth2User.getUserId();
+
+        return new ResponseEntity<>(combinationService.findCombinationBoards(page, sorted, userId), HttpStatus.OK);
+    }
+
     @Operation(summary = "주류 조합 작성", description = "주류 조합 게시판 작성")
     @PostMapping("")
     public ResponseEntity<CombinationBoardCreateResponse> createCombinationBoard(@RequestBody final CombinationBoardCreateRequest combinationBoardCreateRequest,
                                                                                  @AuthUser final CustomOAuth2User customOAuth2User) throws JsonProcessingException {
         Long userId = customOAuth2User.getUserId();
-        objectMapper.setPropertyNamingStrategy(new PropertyNamingStrategies.SnakeCaseStrategy());
-
-        String json = "{\"x_coordinate\":42}";
-        CombinationCreateDto combinationCreateDto = objectMapper.readValue(json, CombinationCreateDto.class);
 
         return new ResponseEntity<>(combinationService.createCombinationBoard(combinationBoardCreateRequest, userId), HttpStatus.CREATED);
     }
